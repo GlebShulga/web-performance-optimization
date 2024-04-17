d73c0a20ae1816a3a1aec9ae227975ec36d3ef77
GlebShulga <GShulga@ITS.JNJ.com>
2024-04-17 11:47:51 +0400

    performance improvement

--- 2-TOOLS/src/App.jsx
+++ 2-TOOLS/src/App.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './Header.jsx';
-import Pages from './Pages.jsx';
-import Card from './Card.jsx';
-import Home from './Home.jsx';
-import model from './model.js';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header.jsx";
+import Pages from "./Pages.jsx";
+import Card from "./Card.jsx";
+import Home from "./Home.jsx";
+import model from "./model.js";
 
-const primary = '#9b5d30';
+const primary = "#9b5d30";
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: {
       main: primary,
-      contrastText: '#fff'
+      contrastText: "#fff",
     },
     secondary: {
-      main: '#000000',
-      contrastText: primary
-    }
-  }
+      main: "#000000",
+      contrastText: primary,
+    },
+  },
 });
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.mineBitcoin(1500);
+    // this.mineBitcoin(1500);
   }
   mineBitcoin(duration) {
     const start = new Date().getTime();
@@ -41,13 +41,17 @@ class App extends React.Component {
       <Router>
         <MuiThemeProvider theme={theme}>
           <div>
-            <Header/>
-            <Route exact path="/" component={Home}/>
-            <Route path="/brrto" component={Pages.Brrto}/>
-            <Route path="/pezza" component={Pages.Pezza}/>
-            <Route path="/fesh" component={Pages.Fesh}/>
-            <Route path="/soop" component={Pages.Soop}/>
-            <Typography variant="body1">Built with 🐟 by Tony the 🐈</Typography>
+            <Header />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/brrto" element={<Pages.Brrto />} />
+              <Route path="/pezza" element={<Pages.Pezza />} />
+              <Route path="/fesh" element={<Pages.Fesh />} />
+              <Route path="/soop" element={<Pages.Soop />} />
+            </Routes>
+            <Typography variant="body1">
+              Built with 🐟 by Tony the 🐈
+            </Typography>
           </div>
         </MuiThemeProvider>
       </Router>
--- 2-TOOLS/src/Card.jsx
+++ 2-TOOLS/src/Card.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import MaterialUICard from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
+import React from "react";
+import MaterialUICard from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    margin: theme.spacing.unit
+    margin: theme.spacing(1),
   },
   card: {
-    width: 300
+    width: 300,
   },
   media: {
     height: 0,
-    paddingTop: '56.25%'
-  }
+    paddingTop: "56.25%",
+  },
 });
 
 class MyCard extends React.Component {
@@ -26,18 +26,24 @@ class MyCard extends React.Component {
     return (
       <div className={this.props.classes.root}>
         <MaterialUICard className={this.props.classes.card}>
-          <CardMedia className={this.props.classes.media}
-            image={this.props.image} />
+          <CardMedia
+            className={this.props.classes.media}
+            image={this.props.image}
+          />
           <CardContent>
-            <Typography gutterBottom variant="headline" component="h2">
+            <Typography gutterBottom variant="h2" component="h2">
               {this.props.title}
             </Typography>
-            <Typography component="p">
-              {this.props.description}
-            </Typography>
+            <Typography component="p">{this.props.description}</Typography>
           </CardContent>
           <CardActions className={this.props.classes.actions}>
-            <Button size="small" color="primary" variant="raised" component={Link} to={this.props.route}>
+            <Button
+              size="small"
+              color="primary"
+              variant="contained"
+              component={Link}
+              to={this.props.route}
+            >
               Pros & Cons
             </Button>
           </CardActions>
