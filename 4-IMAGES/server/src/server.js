ba6a0ba8cbd194460644cff86e579d10e0f59405
GlebShulga <GShulga@ITS.JNJ.com>
2024-04-30 10:32:35 +0400

    Images optimization

--- 4-IMAGES/server/src/server.js
+++ 4-IMAGES/server/src/server.js
@@ -1,30 +1,37 @@
-const express = require('express')
-const resize = require('./resize')
+const express = require("express");
+const resize = require("./resize");
+const path = require("path"); // Import the 'path' module
+const fs = require("fs");
 
-const server = express()
+const server = express();
 
-server.get('/:imageName', (req, res) => {
-    // Extract the query-parameter
-    const imageName = req.params.imageName;
-    const widthString = req.query.width
-    const heightString = req.query.height
-    const format = req.query.format
+server.get("/:imageName", (req, res) => {
+  // Extract the query-parameter
+  const imageName = req.params.imageName;
+  const widthString = req.query.width;
+  const heightString = req.query.height;
+  const format = req.query.format;
 
-    // Parse to integer if possible
-    let width, height
-    if (widthString) {
-        width = parseInt(widthString)
-    }
-    if (heightString) {
-        height = parseInt(heightString)
-    }
-    // Set the content-type of the response
-    res.type(`image/${format || 'png'}`)
+  // Parse to integer if possible
+  let width, height;
+  if (widthString) {
+    width = parseInt(widthString);
+  }
+  if (heightString) {
+    height = parseInt(heightString);
+  }
 
-    // Get the resized image
-    resize(`images/${imageName}.jpeg`, format, width, height).pipe(res)
-})
+  const sanitizedImageName = path.basename(imageName);
+  const imagePath = `images/${sanitizedImageName}.jpeg`;
+
+  if (fs.existsSync(imagePath)) {
+    res.type(`image/${format || "png"}`);
+    resize(imagePath, format, width, height).pipe(res);
+  } else {
+    res.status(404).send("Image not found");
+  }
+});
 
 server.listen(8000, () => {
-    console.log('Server started!')
-})
+  console.log("Server started!");
+});
